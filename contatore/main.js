a23485c74524abb2b4303bfd58d888ec7cb28823
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-08 21:29:39 +0100

    validator + some fixes

--- contatore/main.js
+++ contatore/main.js
@@ -1,16 +1,21 @@
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 let userChoice = prompt("scegli pari o dispari");
-let userChoice2 = prompt("scegli un numero da 1 a 5");
-if (userChoice !== "pari" || userChoice !== "dispari") {
+let userChoice2 = parseInt(prompt("scegli un numero da 1 a 5"));
+if (userChoice !== "pari" && userChoice !== "dispari") {
   alert("stringa non valida scegli tra pari o dispari");
 }
-
+if (userChoice2 > 5 || userChoice2 < 1) {
+  alert("numero non valido");
+}
+if (isNaN(userChoice2) == true) {
+  alert("non è un numero");
+}
 console.log(userChoice);
 console.log(userChoice2);
 const evenOrOdd = evenChoice(userChoice);
 console.log(evenOrOdd);
-const generatedNumber = myNumber(1, 10);
+const generatedNumber = myNumber(1, 5);
 console.log(generatedNumber);
 
 function evenChoice(choice = "pari") {
