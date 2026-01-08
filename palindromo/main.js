8a0c644e6e58f53b7446d76ad9b7724f1cc32ad3
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-08 19:09:40 +0100

    added palindrom verificator

--- palindromo/main.js
+++ palindromo/main.js
@@ -6,6 +6,11 @@ console.log(palindromWord(userWord));
 
 function palindromWord(word) {
   let reversedWord = word.split("").reverse().join("");
+  if (reversedWord === word) {
+    alert("la parola è palindroma");
+  } else {
+    alert("la parola non è palindroma");
+  }
   return reversedWord;
 }
 // Creare una funzione per capire se la parola inserita è palindroma
