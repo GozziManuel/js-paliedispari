abe09d990aa67496e55872e3b05c1a8b1dda623e
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-08 19:24:48 +0100

    pari e dispari

---
+++ contatore/main.js
@@ -0,0 +1,14 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+let userChoice = prompt("scegli pari o dispari");
+let userChoice2 = prompt("scegli un numero da 1 a 5");
+console.log(evenChoice(userChoice));
+function evenChoice(choice = "pari") {
+  const userEven = choice == "pari";
+  return userEven;
+}
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
