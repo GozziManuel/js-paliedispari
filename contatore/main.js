9925cc56131d88b9f5fea97406fa54dedfe9aac9
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-08 20:56:23 +0100

    number generator

--- contatore/main.js
+++ contatore/main.js
@@ -2,12 +2,22 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 let userChoice = prompt("scegli pari o dispari");
 let userChoice2 = prompt("scegli un numero da 1 a 5");
-console.log(evenChoice(userChoice));
+console.log(userChoice);
+console.log(userChoice2);
+const evenOrOdd = evenChoice(userChoice);
+console.log(evenOrOdd);
+const generatedNumber = myNumber(1, 10);
+console.log(generatedNumber);
+
 function evenChoice(choice = "pari") {
   const userEven = choice == "pari";
   return userEven;
 }
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function myNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
 
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
